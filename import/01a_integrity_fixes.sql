6c73fedbaad194fa47d92182f4005b963313a114
emkael <emkael@tlen.pl>
2015-03-03 11:40:29 +0100

    * setting finishing position to NULL where it's zeroed

--- import/01a_integrity_fixes.sql
+++ import/01a_integrity_fixes.sql
@@ -1,2 +1,4 @@
 ALTER TABLE races CHANGE circuitId circuitId INT(11) NULL;
 UPDATE races SET circuitId = NULL WHERE raceId = 935; -- 2015 German GP, venue TBA
+
+UPDATE results SET position = NULL WHERE position = 0;
