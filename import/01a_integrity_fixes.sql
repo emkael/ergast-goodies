e2e9966f64d71e36f66d9a271d65a9f37f484838
emkael <emkael@tlen.pl>
2015-03-20 14:00:12 +0100

    * German GP is no more

--- import/01a_integrity_fixes.sql
+++ import/01a_integrity_fixes.sql
@@ -1,2 +1,2 @@
 ALTER TABLE races CHANGE circuitId circuitId INT(11) NULL;
-UPDATE races SET circuitId = NULL WHERE raceId = 935; -- 2015 German GP, venue TBA
+DELETE FROM races WHERE raceId = 935; -- 2015 German GP is no more
