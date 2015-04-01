4a48faa9e048d07211763a538c350b504162d42c
emkael <emkael@tlen.pl>
2015-04-01 11:42:23 +0200

    * http://ergast.com/mrd/bugs/comment-page-3#comment-12343

--- import/10_fixes.sql
+++ import/10_fixes.sql
@@ -1,1 +1,5 @@
 UPDATE results SET grid = 15 WHERE resultId = 2939; -- Nick Heidfeld, Australia 2000
+
+-- http://ergast.com/mrd/bugs/comment-page-3#comment-12343
+UPDATE results SET position = 3 WHERE resultId = 20185; -- Manny Ayulo, 1951 Indy 500
+UPDATE results SET position = 9, positionText = '9' WHERE resultId = 1207; -- Ralf Schumacher, San Marino 2005
