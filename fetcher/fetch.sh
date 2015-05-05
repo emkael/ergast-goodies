6826ba70039aec1828ca494a54d89c9cd7f7baf8
emkael <emkael@tlen.pl>
2015-05-05 22:38:13 +0200

    * code reformat to meet bashate standards

--- fetcher/fetch.sh
+++ fetcher/fetch.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 pushd $(dirname $0) > /dev/null
 wget -N http://ergast.com/downloads/f1db.sql.gz -q
-if [[ $? != 0 ]]
-then
-    if [[ -f f1db.sql.gz ]]
-    then
+if [[ $? != 0 ]]; then
+    if [[ -f f1db.sql.gz ]]; then
         echo "Warning: could not fetch new DB, using archived version"
     else
         echo "Error: could not fetch DB"
@@ -12,8 +10,7 @@ then
         exit 1
     fi
 fi
-if [[ f1db.sql.gz -nt f1db.sql ]]
-then
+if [[ f1db.sql.gz -nt f1db.sql ]]; then
     echo "Found new ErgastDB version"
     zcat f1db.sql.gz | sed 's/ ENGINE=MyISAM//' > f1db.sql
     find -name f1db.sql.gz -printf '%f: %Td-%Tm-%TY %TH:%TM, %s bytes\n'
--- import/00_base_import.sh
+++ import/00_base_import.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 pushd $(dirname $0) > /dev/null
-if [ ! -f ../fetcher/f1db.sql ]
-then
+if [ ! -f ../fetcher/f1db.sql ]; then
     ../fetcher/fetch.sh
 fi
 < ../fetcher/f1db.sql mysql "$@"
