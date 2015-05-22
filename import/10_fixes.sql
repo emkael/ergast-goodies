3526be9cfee8fb3e2cb674351476b425da007bdc
emkael <emkael@tlen.pl>
2015-05-22 14:57:24 +0200

    * http://ergast.com/mrd/bugs/comment-page-3#comment-12795

---
+++ import/10_fixes.sql
@@ -0,0 +1,29 @@
+UPDATE results SET positionText = '15' WHERE resultId = 1087; -- Albers, China 2006
+
+UPDATE results SET positionText = 'E' WHERE resultId = 8551; -- Larini, San Marino 1988
+UPDATE results SET positionText = 'R' WHERE resultId = 15822; -- Tuero, Luxembourg 1998
+UPDATE results SET positionText = 'R' WHERE resultId = 3977; -- Buettler, Italy 1971
+UPDATE results SET positionText = 'W' WHERE resultId = 14469; -- Donohue, Austria 1975
+UPDATE results SET positionText = 'W' WHERE resultId = 14470; -- Henton, Austria 1975
+UPDATE results SET positionText = 'R' WHERE resultId = 15878; -- Ickx, USA 1971
+UPDATE results SET positionText = 'R' WHERE resultId = 15732; -- Pescarolo, France 1971
+UPDATE results SET positionText = 'N' WHERE resultId = 7408; -- Dalmas, Italy 1990
+UPDATE results SET positionText = 'N' WHERE resultId = 8008; -- Alliot, Mexico 1989
+UPDATE results SET positionText = 'N' WHERE resultId = 8119; -- Gugelmin, France 1989
+UPDATE results SET positionText = 'N' WHERE resultId = 10213; -- Brundle, Australia 1985
+UPDATE results SET positionText = 'N' WHERE resultId = 14616; -- Jarier, Spain 1974
+UPDATE results SET positionText = 'N' WHERE resultId = 15059; -- Beltoise, Belgium 1973
+UPDATE results SET positionText = 'N' WHERE resultId = 19809; -- van der Lof, Netherlands 1952
+UPDATE results SET positionText = 'N' WHERE resultId = 19834; -- Hawthorn, Italy 1952
+UPDATE results SET positionText = 'N' WHERE resultId = 19955; -- Kelly, Great Britain 1951
+UPDATE results SET positionText = 'D' WHERE resultId = 5704; -- de Cesaris, Spain 1993
+UPDATE results SET positionText = 'D' WHERE resultId = 10265; -- Brundle, South Africa 1984
+UPDATE results SET positionText = 'D' WHERE resultId = 10267; -- Bellof, South Africa 1984
+UPDATE results SET positionText = 'D' WHERE resultId = 10463; -- Bellof, Dallas 1984
+UPDATE results SET positionText = 'D' WHERE resultId = 10982; -- Winkelhock, Netherlands 1983
+UPDATE results SET positionText = 'D' WHERE resultId = 18559; -- Magill, Indy 500 1958
+UPDATE results SET positionText = 'D' WHERE resultId = 19792; -- Bonetto, Germany 1952
+UPDATE results SET positionText = '14' WHERE resultId = 1152; -- Sato, Australia 2005
+UPDATE results SET positionText = '11' WHERE resultId = 1149; -- Button, Australia 2005
+UPDATE results SET positionText = 'F' WHERE resultId = 2435; -- Fisichella, France 2002
+UPDATE results SET positionText = 'F' WHERE resultId = 4512; -- Montermini, Spain 1994
