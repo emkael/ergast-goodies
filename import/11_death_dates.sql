f7a72e4778787a90f823a4b21d28982997b95e59
emkael <emkael@tlen.pl>
2019-05-25 11:22:13 +0200

    RIP, Niki

--- import/11_death_dates.sql
+++ import/11_death_dates.sql
@@ -1,4 +1,5 @@
 ALTER TABLE drivers ADD dod DATE NULL AFTER dob;
+UPDATE drivers SET dod = "2019-05-20" WHERE driverId = 182; -- Niki Lauda
 UPDATE drivers SET dod = "2019-04-29" WHERE driverId = 446; -- Carlo Maria Abate
 UPDATE drivers SET dod = "2019-04-24" WHERE driverId = 365; -- Hubert Hahne
 UPDATE drivers SET dod = "2018-11-08" WHERE driverId = 301; -- Dave Morgan
