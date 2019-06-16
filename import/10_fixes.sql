1d7dba3c4a4d62b9cff9f8d1e68d7b5c4253125f
emkael <emkael@tlen.pl>
2019-06-16 22:50:26 +0200

    http://ergast.com/mrd/bugs/comment-page-7/#comment-34866

--- import/10_fixes.sql
+++ import/10_fixes.sql
@@ -1,17 +1,1 @@
--- Q1
--- 2008 Bahrain GP, Vettel, Sutil
-UPDATE qualifying SET q1 = '1:33.562' WHERE qualifyId = 63;
-UPDATE qualifying SET q1 = '1:33.845' WHERE qualifyId = 64;
--- 1998 Brazilian GP, Magnussen
-UPDATE qualifying SET q1 = '1:19.644' WHERE qualifyId = 1693;
--- 1997 Brazilian GP, Panis
-UPDATE qualifying SET q1 = '1:16.756' WHERE qualifyId = 1835;
--- 2012 British GP, Karthikeyan
-UPDATE qualifying SET q1 = '1:53.040' WHERE qualifyId = 5178;
--- 2014 German GP, Bianchi
-UPDATE qualifying SET q1 = '1:19.676' WHERE qualifyId = 6099;
--- Q2
--- 2011 Monaco GP, Di Resta
-UPDATE qualifying SET q2 = '1:16.118' WHERE qualifyId = 4669;
--- 2013 Singapore GP, Raikkonen
-UPDATE qualifying SET q2 = '1:44.658' WHERE qualifyId = 5718;
+
