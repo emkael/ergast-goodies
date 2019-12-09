bb4733aa013d17d549999a08fce63ea2d5b4cd55
emkael <emkael@tlen.pl>
2019-12-09 18:55:58 +0100

    RIP, Ken Kavanagh

--- import/11_death_dates.sql
+++ import/11_death_dates.sql
@@ -1,4 +1,5 @@
 ALTER TABLE drivers ADD dod DATE NULL AFTER dob;
+UPDATE drivers SET dod = "2019-11-26" WHERE driverId = 582; -- Ken Kavanagh
 UPDATE drivers SET dod = "2019-10-12" WHERE driverId = 330; -- Nanni Galli
 UPDATE drivers SET dod = "2019-09-30" WHERE driverId = 457; -- Ben Pon
 UPDATE drivers SET dod = "2019-06-02" WHERE driverId = 421; -- Alan Rollinson
