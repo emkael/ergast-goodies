0027916f526004aa52f72051a70f9f696a097ab9
emkael <emkael@tlen.pl>
2020-03-31 12:36:13 +0200

    Phantom WC standings removed pre-foreign keys

--- import/01a_integrity_fixes.sql
+++ import/01a_integrity_fixes.sql
@@ -1,3 +1,6 @@
 UPDATE qualifying SET q1 = NULL WHERE q1 = '';
 UPDATE qualifying SET q2 = NULL WHERE q2 = '';
 UPDATE qualifying SET q3 = NULL WHERE q3 = '';
+
+DELETE FROM constructorStandings WHERE raceId NOT IN (SELECT raceId FROM races);
+DELETE FROM driverStandings WHERE raceId NOT IN (SELECT raceId FROM races);
