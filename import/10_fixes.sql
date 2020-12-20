6d153df577d606b8d7606dd9838f64143274951e
emkael <emkael@tlen.pl>
2020-12-21 00:24:09 +0100

    Ocon's and Stroll's race times for Abu Dhabi '20

--- import/10_fixes.sql
+++ import/10_fixes.sql
@@ -1,1 +1,3 @@
-
+-- http://ergast.com/mrd/bugs/comment-page-8/#comment-145868
+UPDATE results SET `time` = '+1:41.069', milliseconds = 5889714 WHERE resultId = 24954; -- Ocon, Abu Dhabi 2020
+UPDATE results SET `time` = '+1:42.738', milliseconds = 5891383 WHERE resultId = 24955; -- Stroll, Abu Dhabi 2020
